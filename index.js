fa7e5517963292949a4e423428b966726adff50c
Ankit-dev1771 <ankitsingh2481@gmail.com>
2025-08-14 12:37:35 +0530

    update ping

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
 import WebSocket, { WebSocketServer } from 'ws';
 
-// Start WebSocket server on port 8080
-const wss = new WebSocketServer({ server: httpServer  }, () => {
-    console.log("WebSocket server running on ws://localhost:8080");
-});
+const wss = new WebSocketServer({ port: 8080 });
 
 wss.on('connection', (ws) => {
-    console.log("Player connected");
-
     ws.on('message', (message) => {
-        const msg = message.toString();
-
-        if (msg === "ping") {
-            // Respond immediately to measure round-trip time
-            ws.send("pong");
+        const data = JSON.parse(message);
+        if (data.type === "ping") {
+            ws.send(JSON.stringify({ type: "pong" }));
         }
-    });
-
-    ws.on('close', () => {
-        console.log("Player disconnected");
     });
 });
