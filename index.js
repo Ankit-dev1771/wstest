f7adc6e07a5ab05a7b6a98f167a1ee77c7f0be32
Ankit-dev1771 <ankitsingh2481@gmail.com>
2025-08-14 11:58:57 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import { WebSocketServer } from 'ws';
+
+// Create WebSocket server on port 8080
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', (ws) => {
+    console.log('Player connected');
+
+    // When server gets a message from client
+    ws.on('message', (message) => {
+        console.log('Received:', message.toString());
+
+        // Broadcast to all connected players
+        wss.clients.forEach((client) => {
+            if (client.readyState === ws.OPEN) {
+                client.send(message.toString());
+            }
+        });
+    });
+
+    ws.on('close', () => {
+        console.log('Player disconnected');
+    });
+});
+
+console.log("WebSocket server running on ws://localhost:8080");
